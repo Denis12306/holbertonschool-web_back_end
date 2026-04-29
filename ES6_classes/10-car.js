79d1823995b96152a1d35042778fc0e423eb216c
Denis12306 <denis.gavaud@gmail.com>
2026-04-29 10:00:36 +0200

    chore: add package.json with scripts and devDependencies for ES6 classes project

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,27 @@
+const cloneSymbol = Symbol('clone');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+
+    this[cloneSymbol] = this.constructor;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new this[cloneSymbol]();
+  }
+}
