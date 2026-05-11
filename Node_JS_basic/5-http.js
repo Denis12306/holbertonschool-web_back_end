54001f3682c5d70692c9f0232cd3b90813a57d7a
Denis12306 <denis.gavaud@gmail.com>
2026-05-11 15:47:52 +0200

    fix: improve response handling and status codes in HTTP server

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,15 +4,20 @@ const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
+    res.statusCode = 200;
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     countStudents(database)
-      .then(() => {
-        res.end('This is the list of our students');
+      .then((data) => {
+        res.statusCode = 200;
+        res.end(`This is the list of our students\n${data}`);
       })
       .catch((err) => {
-        res.end(err.message);
+        res.statusCode = 500;
+        res.end(`This is the list of our students\n${err.message}`);
       });
   }
 });
